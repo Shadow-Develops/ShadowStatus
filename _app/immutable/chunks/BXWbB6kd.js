a069747a7585bcb1ffe175a6451694ce8781f9c7
AgentBUB <AgentBUB@users.noreply.github.com>
2026-01-22 15:35:42 +0000

    deploy: 8b85b30641262163e610cb28bfbe8a988c0f63b0

--- _app/immutable/chunks/BXWbB6kd.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./ZI2f2Mto.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
