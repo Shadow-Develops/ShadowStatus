7ac06bc0a0a1e1bccbd74eaea5f237eb243fbbdb
AgentBUB <AgentBUB@users.noreply.github.com>
2026-01-23 16:57:07 +0000

    deploy: d0bc98f0a42c5d72cb6114e4d82909b4e0819dca

--- _app/immutable/chunks/D5LlM_GU.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./Bg9SRCk_.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
