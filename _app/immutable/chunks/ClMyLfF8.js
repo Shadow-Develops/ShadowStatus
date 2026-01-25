339b5afda9b79f0b417256c31510125f6c390224
AgentBUB <AgentBUB@users.noreply.github.com>
2026-01-25 08:40:38 +0000

    deploy: 60495dca515a5ca2ec21de81576cd2fd13e89293

--- _app/immutable/chunks/ClMyLfF8.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./CX5Xm8pP.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
