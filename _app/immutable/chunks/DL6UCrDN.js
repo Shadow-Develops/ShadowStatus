210e3c47b7f31d4c791b31402866a001b02d282a
AgentBUB <AgentBUB@users.noreply.github.com>
2026-01-25 15:10:32 +0000

    deploy: a6b6a8d81134c65af150821ebbdbc6ec0a24aac9

---
+++ _app/immutable/chunks/DL6UCrDN.js
@@ -0,0 +1,1 @@
+import{s as e}from"./et5IV_Af.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
