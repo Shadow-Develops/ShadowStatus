9818896289455465aaf3aa2c83408c1c739222a9
AgentBUB <AgentBUB@users.noreply.github.com>
2026-01-25 21:35:35 +0000

    deploy: 3d292a4892305f3f11e9a08aa4242570748af16c

--- _app/immutable/chunks/mNUqX91L.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./JQS2Z-33.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
