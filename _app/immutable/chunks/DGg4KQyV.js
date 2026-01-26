0e28f5b5d06a6a18dd3d5a62f5afff8d8d5c2d06
AgentBUB <AgentBUB@users.noreply.github.com>
2026-01-26 07:55:28 +0000

    deploy: f3f21cc41b060be551053e04b5a0968961ac13bf

---
+++ _app/immutable/chunks/DGg4KQyV.js
@@ -0,0 +1,1 @@
+import{s as e}from"./EpV-_pDR.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
