b67e26d13c5d76b9c8ce71cc87de09394480766c
AgentBUB <AgentBUB@users.noreply.github.com>
2026-01-28 22:15:40 +0000

    deploy: 33667d9d4ab2f50f728e1b3c896807d52d2370e6

--- _app/immutable/chunks/CZKmUsvw.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./CRU2rQIN.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
