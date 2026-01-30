80ab05c15f00d9c08efdeffe999c6c9742667abb
AgentBUB <AgentBUB@users.noreply.github.com>
2026-01-30 07:55:28 +0000

    deploy: 7b47114f0b2ad79616011c7290be277144d4adfa

--- _app/immutable/chunks/CsXLxP-9.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./DV82l3uJ.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
