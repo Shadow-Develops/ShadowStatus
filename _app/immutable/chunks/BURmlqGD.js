b8c1c65b29c42e83a7a614bc8666707b4341494e
AgentBUB <AgentBUB@users.noreply.github.com>
2026-01-30 10:00:34 +0000

    deploy: 55a7fece80a5c94905f1d1475285af44df85c19e

--- _app/immutable/chunks/BURmlqGD.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./ul_aY6Os.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
