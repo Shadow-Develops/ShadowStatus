a68645cf61ab0ff8f05213262a66f492199e87c5
AgentBUB <AgentBUB@users.noreply.github.com>
2026-02-01 18:35:30 +0000

    deploy: dce2f1dc418acc78b76af6ac90cd64d964c8d164

--- _app/immutable/chunks/BFThUnrB.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./755yd3iN.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
