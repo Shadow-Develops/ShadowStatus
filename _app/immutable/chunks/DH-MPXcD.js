9a7150121735b92fccabf128a6df0ac3105bc746
AgentBUB <AgentBUB@users.noreply.github.com>
2026-02-04 15:05:37 +0000

    deploy: 322e9ab94fcef1e22bf6be57c342fabc90c7031a

---
+++ _app/immutable/chunks/DH-MPXcD.js
@@ -0,0 +1,1 @@
+import{s as e}from"./DcBc9h-I.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
