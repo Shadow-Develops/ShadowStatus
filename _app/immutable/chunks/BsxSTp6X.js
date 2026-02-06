b8d4aec207efde29dd97195bb39c93cfbe89a63e
AgentBUB <AgentBUB@users.noreply.github.com>
2026-02-06 13:35:36 +0000

    deploy: c914984dce67c33a6d179ca868de26170d626a8c

--- _app/immutable/chunks/BsxSTp6X.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./_czuotHd.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
