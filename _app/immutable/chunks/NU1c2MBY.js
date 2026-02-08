660975678e8e440f1c4ad3a1f86499cd9fed6c24
AgentBUB <AgentBUB@users.noreply.github.com>
2026-02-08 20:40:36 +0000

    deploy: 3b090fe598edbdc09d302d64a9a9d78106d18eab

---
+++ _app/immutable/chunks/NU1c2MBY.js
@@ -0,0 +1,1 @@
+import{s as e}from"./qMYr5ZoW.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
