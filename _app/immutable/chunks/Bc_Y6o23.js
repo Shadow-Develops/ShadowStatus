ed45e02d9b09f3606df20c6457362681037b01d6
AgentBUB <AgentBUB@users.noreply.github.com>
2026-02-10 12:30:40 +0000

    deploy: 52fabcf2ef18ef6a70fe25bcff295588ae4eff82

--- _app/immutable/chunks/Bc_Y6o23.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./DW41OblA.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
