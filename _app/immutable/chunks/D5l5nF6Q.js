6c448136685ca50721bdc722ba74ef9eef7c99a8
AgentBUB <AgentBUB@users.noreply.github.com>
2026-02-12 13:10:43 +0000

    deploy: bf8168e8a7445cce229d80910880370e5e497e26

--- _app/immutable/chunks/D5l5nF6Q.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./CSl4t62v.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
