ae185c19015b6ab4c27c3535dfddda06f35ac03d
AgentBUB <AgentBUB@users.noreply.github.com>
2026-02-13 08:45:35 +0000

    deploy: f868cfd095710b5e223fe67a274646a16138c679

---
+++ _app/immutable/chunks/vMs5SQWE.js
@@ -0,0 +1,1 @@
+import{s as e}from"./BtXcu_qQ.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
