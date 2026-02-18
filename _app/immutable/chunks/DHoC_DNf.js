51580a2544182c1e048db2074d86d1ca6dbdb32d
AgentBUB <AgentBUB@users.noreply.github.com>
2026-02-18 11:05:30 +0000

    deploy: 264e0e34f2438690752f2a82ba78e6ed8dfd50c2

---
+++ _app/immutable/chunks/DHoC_DNf.js
@@ -0,0 +1,1 @@
+import{s as e}from"./D4lm96q6.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
