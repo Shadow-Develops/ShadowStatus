e06d030f5d00bd25042e88ae8e4283ec0e493d4c
AgentBUB <AgentBUB@users.noreply.github.com>
2026-02-24 07:20:37 +0000

    deploy: 0102ae78f77136b82114741eb23c6282351fff22

---
+++ _app/immutable/chunks/DGFawQif.js
@@ -0,0 +1,1 @@
+import{s as e}from"./bO9e-pkV.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
