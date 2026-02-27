48ce946d8fb49b5fa98064e0487d3c214956ad16
AgentBUB <AgentBUB@users.noreply.github.com>
2026-02-27 02:05:40 +0000

    deploy: 3e7aecd7e47d12fc375470e437c4e4f3ef57448c

--- _app/immutable/chunks/B8uqHwFw.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./Bpi4_ZQl.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
