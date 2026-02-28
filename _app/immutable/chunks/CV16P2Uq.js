3257d9650cca54e74e35041a01782998ad35d72f
AgentBUB <AgentBUB@users.noreply.github.com>
2026-02-28 17:05:38 +0000

    deploy: 04f17744015d8e182779b5c711a8b9020c29b309

---
+++ _app/immutable/chunks/CV16P2Uq.js
@@ -0,0 +1,1 @@
+import{s as e}from"./Cma_4KF0.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
