b53d461cf80075ce72f7b036ec25640e74d00d98
AgentBUB <AgentBUB@users.noreply.github.com>
2026-03-04 14:20:36 +0000

    deploy: be9d5b22456ed1baffac0611062c782aa01ab8a7

---
+++ _app/immutable/chunks/CTlOazOb.js
@@ -0,0 +1,1 @@
+import{s as e}from"./BCrF-DTf.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
