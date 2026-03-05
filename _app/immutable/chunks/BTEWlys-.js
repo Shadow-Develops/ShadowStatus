4d76b1fe337439b466526dbcfe50260d9eb15f44
AgentBUB <AgentBUB@users.noreply.github.com>
2026-03-05 00:55:56 +0000

    deploy: 20b9405955dc5c297a29286abb19d6b3a1bb9e4c

---
+++ _app/immutable/chunks/BTEWlys-.js
@@ -0,0 +1,1 @@
+import{s as e}from"./BxB3aQJ2.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
