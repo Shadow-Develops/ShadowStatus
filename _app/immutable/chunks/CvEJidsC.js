acebbdd479c2620c6c1a4ce9db8c417592264457
AgentBUB <AgentBUB@users.noreply.github.com>
2026-03-09 15:51:01 +0000

    deploy: a775ec0e6e4afac2763707edefda7b96318da6e5

---
+++ _app/immutable/chunks/CvEJidsC.js
@@ -0,0 +1,1 @@
+import{s as e}from"./CB9-3f7u.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
