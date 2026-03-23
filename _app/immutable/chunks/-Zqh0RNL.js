f77377fb06df4652441a18ca5de9a2c52535824c
AgentBUB <AgentBUB@users.noreply.github.com>
2026-03-23 15:35:35 +0000

    deploy: 0d15df7b09ec2afeb1b8aeea34fa68e1ae572d2c

--- _app/immutable/chunks/-Zqh0RNL.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./Dxd0MXS2.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
