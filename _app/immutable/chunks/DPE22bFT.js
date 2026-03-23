cc2de240e84e11e62ed1b6a6586cd4a7cc3620e8
AgentBUB <AgentBUB@users.noreply.github.com>
2026-03-23 13:55:39 +0000

    deploy: a373d69b6fe10012cd2686cfbb8b6bce7084b570

---
+++ _app/immutable/chunks/DPE22bFT.js
@@ -0,0 +1,1 @@
+import{s as e}from"./oH1_8fAc.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
