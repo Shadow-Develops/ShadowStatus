63ef54598440db6f6cc5292b7a3e484d8999f014
AgentBUB <AgentBUB@users.noreply.github.com>
2026-03-24 11:05:32 +0000

    deploy: 648b85bfb6dbdcd598196b78a4af1da86a866577

---
+++ _app/immutable/chunks/DK1XlhM3.js
@@ -0,0 +1,1 @@
+import{s as e}from"./w2a08Pen.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
