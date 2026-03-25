93a0ae760ea6377049078ee5c7178d1a6e93fa2d
AgentBUB <AgentBUB@users.noreply.github.com>
2026-03-25 20:10:34 +0000

    deploy: 1a01ddbdc1d8f86163321b3a2b0cc702957c8c34

---
+++ _app/immutable/chunks/x-WpS5QA.js
@@ -0,0 +1,1 @@
+import{s as e}from"./Dw35Q2pg.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
