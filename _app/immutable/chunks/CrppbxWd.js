3b8d9f877e077130736f8c1c69f74f4889623818
AgentBUB <AgentBUB@users.noreply.github.com>
2026-03-28 04:00:34 +0000

    deploy: f0415e4f906a15a69649f11627f108fc6e9ae9aa

---
+++ _app/immutable/chunks/CrppbxWd.js
@@ -0,0 +1,1 @@
+import{s as e}from"./DWnp0yYZ.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
