cf9e504bae263d4991678e3948f343c394fafd35
AgentBUB <AgentBUB@users.noreply.github.com>
2026-03-28 04:55:29 +0000

    deploy: ac2d7a8a9dd5f16bc532b017db5d6ee96c2611b3

--- _app/immutable/chunks/WD0z67zS.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./CSKsu5wC.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
