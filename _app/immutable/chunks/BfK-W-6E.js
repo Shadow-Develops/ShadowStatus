eeb79d0c4d2cecadfa65f86d4e9a31b30786f028
AgentBUB <AgentBUB@users.noreply.github.com>
2026-04-01 08:15:38 +0000

    deploy: 6bcd4999c9d564c5b2fcdf1d63e4523217dc900f

---
+++ _app/immutable/chunks/BfK-W-6E.js
@@ -0,0 +1,1 @@
+import{s as e}from"./tmuZ-Wy-.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
