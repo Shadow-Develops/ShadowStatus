d8830659102e782299bdbe2d0cb698393175eb23
AgentBUB <AgentBUB@users.noreply.github.com>
2026-04-01 17:25:47 +0000

    deploy: 319cdc902a935d483c61583f51f4779b28898f81

---
+++ _app/immutable/chunks/DrK2WW45.js
@@ -0,0 +1,1 @@
+import{s as e}from"./o4tEtan5.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
