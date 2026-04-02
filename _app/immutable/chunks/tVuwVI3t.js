08bfddd4e4f9f353aeeb0d70c8109c442870250b
AgentBUB <AgentBUB@users.noreply.github.com>
2026-04-02 00:20:37 +0000

    deploy: 5296c33de143c70c4d6e733881d9038bdd1ef0d9

---
+++ _app/immutable/chunks/tVuwVI3t.js
@@ -0,0 +1,1 @@
+import{s as e}from"./Ibke4-iS.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
