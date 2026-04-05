c202c45df32751e30d4cb394582be610e51a8843
AgentBUB <AgentBUB@users.noreply.github.com>
2026-04-05 23:50:29 +0000

    deploy: 367210ced495b7110d5d3568e1646452652cf562

---
+++ _app/immutable/chunks/ZO5E7QrA.js
@@ -0,0 +1,1 @@
+import{s as e}from"./CHc14Mvi.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
