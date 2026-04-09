d16fdbb83c9140a8455cc72f00a839bd708c80d2
AgentBUB <AgentBUB@users.noreply.github.com>
2026-04-09 10:15:34 +0000

    deploy: d2299426fefbac0f00e3018617bc1aea25e313cb

--- _app/immutable/chunks/B5JIGnGR.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./BMR0_oQd.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
