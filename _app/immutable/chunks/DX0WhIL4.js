a9b08f44434459dc7a5f5c675959c13050fe7970
AgentBUB <AgentBUB@users.noreply.github.com>
2026-04-27 17:11:27 +0000

    deploy: c301f672d1fcc514813a8f79837d1d652ae64f59

---
+++ _app/immutable/chunks/DX0WhIL4.js
@@ -0,0 +1,1 @@
+import{s as e}from"./C91H_HXR.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
