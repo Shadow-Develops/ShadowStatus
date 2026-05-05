8f8628e408627402491aff9e75daf004f4f20052
AgentBUB <AgentBUB@users.noreply.github.com>
2026-05-05 08:01:01 +0000

    deploy: 2a85720c71b5f9547acf682246d0a38e24c4c075

--- _app/immutable/chunks/DNjC1TAy.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./Cxie9h7x.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
