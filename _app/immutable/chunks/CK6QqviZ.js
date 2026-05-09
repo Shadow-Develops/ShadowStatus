92629ecb4222bd7b2f6afb6ef1339aa0461e6836
AgentBUB <AgentBUB@users.noreply.github.com>
2026-05-09 10:00:51 +0000

    deploy: a7dad0c850d20fcd265d700ae9ad0883217f5c69

---
+++ _app/immutable/chunks/CK6QqviZ.js
@@ -0,0 +1,1 @@
+import{s as e}from"./CGpA88PY.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
