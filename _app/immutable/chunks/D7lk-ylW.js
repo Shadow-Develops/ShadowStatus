e1bc2bc0828223147c7d5282aa1e732be501c961
AgentBUB <AgentBUB@users.noreply.github.com>
2026-05-10 09:00:41 +0000

    deploy: f62ae0d78a5cfd3f7428395009e115f168699676

---
+++ _app/immutable/chunks/D7lk-ylW.js
@@ -0,0 +1,1 @@
+import{s as e}from"./mx_0B4tE.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
