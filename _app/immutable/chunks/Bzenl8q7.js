585937e0360d6798e85e0dba5c3e05d21191139e
AgentBUB <AgentBUB@users.noreply.github.com>
2026-06-05 14:45:50 +0000

    deploy: ee922713633e6f03ddfef734778946e9d03220e2

--- _app/immutable/chunks/Bzenl8q7.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./CGX_nDjl.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
