acd3912af67ad902a0e926f3ceb5363507c4f723
AgentBUB <AgentBUB@users.noreply.github.com>
2026-06-16 23:15:55 +0000

    deploy: 6723da218286531489698cfe647855fe8013e90b

--- _app/immutable/chunks/bindSAkb.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./Cml5LlpV.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
