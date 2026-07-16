034997e20db68ea99b473c4f7f392264e8a8d411
AgentBUB <AgentBUB@users.noreply.github.com>
2026-07-16 04:45:35 +0000

    deploy: 03faa6496d025a84832a523d137ee36d1a9c8e08

---
+++ _app/immutable/chunks/DANV_srM.js
@@ -0,0 +1,1 @@
+import{s as e}from"./UoF59ICN.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
