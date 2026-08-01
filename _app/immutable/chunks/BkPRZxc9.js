1ce2a9184f180a95f5ab43e8633345cdc8c2ad26
AgentBUB <AgentBUB@users.noreply.github.com>
2026-08-01 19:55:38 +0000

    deploy: ad874d9b1e46248410f84b8b0d87e78b2d92a24f

--- _app/immutable/chunks/BkPRZxc9.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./BbLm9zJ_.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
