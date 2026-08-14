62d056cdb993d8459f40cbdc84dea00b8af3eedd
AgentBUB <AgentBUB@users.noreply.github.com>
2026-08-14 14:00:45 +0000

    deploy: 523d1d4b686fbe4f72ae09943b576ae5e4e02388

---
+++ _app/immutable/chunks/BowRAm-N.js
@@ -0,0 +1,1 @@
+import{s as e}from"./CJK4g6vl.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
