927d42c7b2b2017f68023429596ee332484471d5
AgentBUB <AgentBUB@users.noreply.github.com>
2026-08-20 16:15:42 +0000

    deploy: c94a1ab585d8bece06b0d8394435452f25abc065

---
+++ _app/immutable/chunks/DEZk1DXT.js
@@ -0,0 +1,1 @@
+import{s as e}from"./FZ-7Dh28.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
