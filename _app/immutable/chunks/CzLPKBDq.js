cddd76ccf6a5fe9f8cb83ba2b9798796db3ae292
AgentBUB <AgentBUB@users.noreply.github.com>
2026-08-23 04:33:06 +0000

    deploy: cabaca9cb38c391d8307ed6e4172cd1ecc41c4a4

---
+++ _app/immutable/chunks/CzLPKBDq.js
@@ -0,0 +1,1 @@
+import{s as e}from"./DCQ3g_vW.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
