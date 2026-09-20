84339da9297221e25c10f680ab8be0955afbde84
AgentBUB <AgentBUB@users.noreply.github.com>
2026-09-20 10:15:32 +0000

    deploy: 2de5806e69182d0a6697223c3c76e7b69bd793c9

--- _app/immutable/chunks/B3bSKoPO.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./CR4F24C7.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
